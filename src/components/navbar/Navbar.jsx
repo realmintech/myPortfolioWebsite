b7b19828d461c87f4653cabec53767068f4c25f7
Mariam Adesina <adesinamariam7@gmail.com>
2024-05-02 03:42:55 +0100

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,52 +7,54 @@ export default function Navbar() {
     <>
       <nav className="navbar navbar-expand-sm navbar-dark sticky-top">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <div className="col-lg-6 col-md-6">
             <img
               src={myPic}
               alt=""
               style={{ width: '50px', height: '50px', borderRadius: '50%' }}
             />
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapsibleNavbar"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="collapsibleNavbar">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link" href="#home">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#about">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#project">
-                  Projects
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#experience">
-                  Experience
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#contact">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          <div className="mx-5 iconMode">
-            {/* <FaMoon fontSize={25} /> */}
           </div>
+          <div className="col-lg-6 col-md-6">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapsibleNavbar"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="collapsibleNavbar">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link" href="#home">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#about">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#project">
+                    Projects
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#experience">
+                    Experience
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#contact">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+              <div className="mx-5 iconMode">
+                {/* <FaMoon fontSize={25} /> */}
+              </div>
+            </div>
           </div>
         </div>
       </nav>
